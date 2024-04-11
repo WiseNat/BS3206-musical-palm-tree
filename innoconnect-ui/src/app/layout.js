6eba0cbf04836636b7c8c87a00421614650f2d89
WiseNat <nathan88wise@gmail.com>
2024-04-11 16:17:02 +0100

    feat: mui styling integration

--- innoconnect-ui/src/app/layout.js
+++ innoconnect-ui/src/app/layout.js
@@ -1,4 +1,6 @@
 import { Inter } from "next/font/google";
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme';
 import "./ui/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -11,7 +13,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeProvider theme={theme}>
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
