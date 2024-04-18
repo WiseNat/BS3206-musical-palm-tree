9e53b48ab95c8aa94eda4f8b6188c347809b9919
WiseNat <nathan88wise@gmail.com>
2024-04-18 12:28:10 +0100

    feat: navbar component

---
+++ innoconnect-ui/src/app/components/Navbar.js
@@ -0,0 +1,30 @@
+"use client";
+import AppBar from "@mui/material/AppBar";
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import AccountTreeOutlinedIcon from '@mui/icons-material/AccountTreeOutlined';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+
+const Navbar = ({ }) => {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+    <AppBar position="static">
+      <Toolbar variant="dense">
+        <IconButton color="inherit">  {/* TODO: Make this link to home page */}
+          <AccountTreeOutlinedIcon/>
+        </IconButton>
+          <Typography className="select-none" variant="h7" color="inherit" component="div" sx={{ flexGrow: 1, ml: 1 }}>
+            <b>InnoConnect</b>
+          </Typography>
+        <IconButton color="inherit"> {/* TODO: Make this link to account page */}
+          <AccountCircleOutlinedIcon />
+        </IconButton>
+      </Toolbar>
+    </AppBar>
+  </Box>
+  );
+};
+
+export default Navbar;
