81c9715caa4a494ec6b7053942a35ec5856e0abe
Tom Shortridge <tomshortridge@Toms-MacBook-Pro-2.local>
2024-05-02 09:35:37 +0100

    feat: adding log out functionality

--- innoconnect-ui/src/app/components/Navbar.js
+++ innoconnect-ui/src/app/components/Navbar.js
@@ -1,29 +1,74 @@
 "use client";
 import AppBar from "@mui/material/AppBar";
-import Box from '@mui/material/Box';
-import IconButton from '@mui/material/IconButton';
+import Box from "@mui/material/Box";
+import IconButton from "@mui/material/IconButton";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import AccountTreeOutlinedIcon from '@mui/icons-material/AccountTreeOutlined';
-import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import AccountTreeOutlinedIcon from "@mui/icons-material/AccountTreeOutlined";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import { useState } from "react";
+import { signOut } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
-const Navbar = ({ }) => {
+const Navbar = ({}) => {
+  const router = useRouter();
+  const [show, setShow] = useState(null);
+
+  const handleClick = (e) => {
+    setShow(e.currentTarget);
+  };
+
+  const handleClose = () => {
+    setShow(null);
+  };
+
+  const handleLogOut = async () => {
+    await signOut("credentials");
+    router.push("/");
+  };
+
+  const handleProfile = () => {
+    router.push("/ui/profile");
+  };
+
   return (
     <Box sx={{ flexGrow: 1 }}>
-    <AppBar position="static">
-      <Toolbar variant="dense">
-        <IconButton color="inherit" href="/" >
-          <AccountTreeOutlinedIcon/>
-        </IconButton>
-          <Typography className="select-none" variant="h7" sx={{ flexGrow: 1, ml: 1 }}>
+      <AppBar position="static">
+        <Toolbar variant="dense">
+          <IconButton color="inherit" href="/">
+            <AccountTreeOutlinedIcon />
+          </IconButton>
+          <Typography
+            className="select-none"
+            variant="h7"
+            sx={{ flexGrow: 1, ml: 1 }}
+          >
             <b>InnoConnect</b>
           </Typography>
-        <IconButton color="inherit"> {/* TODO: Make this link to account page */}
-          <AccountCircleOutlinedIcon />
-        </IconButton>
-      </Toolbar>
-    </AppBar>
-  </Box>
+          <IconButton color="inherit" onClick={handleClick}>
+            {" "}
+            {/* TODO: Make this link to account page */}
+            <AccountCircleOutlinedIcon />
+          </IconButton>
+          <Menu
+            id="account-menu"
+            anchorEl={show}
+            keepMounted
+            open={Boolean(show)}
+            onClose={handleClose}
+          >
+            <MenuItem onClick={handleProfile} key="Profile" value="Profile">
+              Profile
+            </MenuItem>
+            <MenuItem onClick={handleLogOut} key="Log Out" value="Log Out">
+              Log Out
+            </MenuItem>
+          </Menu>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 };
 
