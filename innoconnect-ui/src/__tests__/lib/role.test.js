a2685b390fba56e44e479575dfb0cb9d8272e4b3
WiseNat <nathan88wise@gmail.com>
2024-05-13 22:24:54 +0100

    test: getRoleIcon

--- innoconnect-ui/src/__tests__/lib/role.test.js
+++ innoconnect-ui/src/__tests__/lib/role.test.js
@@ -4,10 +4,48 @@ import { render, screen } from '@testing-library/react';
 // use the following for a great debugger: screen.logTestingPlaygroundURL()
 
 describe("getRoleIcon", () => {
-    it("returns StarIcon when given 'founder'", () => {
-        render(getRoleIcon("founder"))
+    it.each(
+        ["founder", "Founder", "FoUnDer"]
+    )("returns StarIcon when given '%s'", (input) => {
+        render(getRoleIcon(input))
 
         const element = screen.getByTestId("StarIcon");
+
+        expect(element).toBeTruthy()
+    });
+    it.each(
+        ["developer", "Developer", "DevElopEr"]
+    )("returns CodeIcon when given '%s'", (input) => {
+        render(getRoleIcon(input))
+
+        const element = screen.getByTestId("CodeIcon");
+
+        expect(element).toBeTruthy()
+    });
+    it.each(
+        ["quality assurance", "Quality Assurance", "qualItY aSSURance"]
+    )("returns AssignmentTurnedInIcon when given '%s'", (input) => {
+        render(getRoleIcon(input))
+
+        const element = screen.getByTestId("AssignmentTurnedInIcon");
+
+        expect(element).toBeTruthy()
+    });
+    it.each(
+        ["project manager", "Project Manager", "prOjecT mAnagEr"]
+    )("returns GroupsIcon when given '%s'", (input) => {
+        render(getRoleIcon(input))
+
+        const element = screen.getByTestId("GroupsIcon");
+
+        expect(element).toBeTruthy()
+    });
+    it.each(
+        ["Software Architect", "Builder", "Scrum Master"]
+    )("returns HelpOutlineIcon when given an unknown value '%s'", (input) => {
+        render(getRoleIcon(input))
+
+        const element = screen.getByTestId("HelpOutlineIcon");
 
         expect(element).toBeTruthy()
     });
