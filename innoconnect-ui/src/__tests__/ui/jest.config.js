2752bdd10b1ba705551c9e7afe59d02bb95c2d4f
WiseNat <nathan88wise@gmail.com>
2024-05-19 16:28:58 +0100

    chore: db connection mock

--- innoconnect-ui/src/__tests__/ui/jest.config.js
+++ innoconnect-ui/src/__tests__/ui/jest.config.js
@@ -8,7 +8,13 @@ const createJestConfig = nextJest({
 const config = {
     clearMocks: true,
     testEnvironment: "jsdom",
-    testPathIgnorePatterns: ["/node_modules", "jest.config.js", "lib.js"],
+    testPathIgnorePatterns: ["/node_modules", "jest.config.js", "lib.js", "setupFile.js"],
+    setupFilesAfterEnv: [
+      "<rootDir>/setupFile.js"
+    ],
+    moduleNameMapper: {
+      '^@/(.*)$': '<rootDir>/../../$1'
+    },
 };
   
 module.exports = createJestConfig(config);
--- innoconnect-ui/src/__tests__/ui/setupFile.js
+++ innoconnect-ui/src/__tests__/ui/setupFile.js
@@ -1,11 +1,7 @@
-import * as mongoose from 'mongoose';
-
 beforeAll(async () => {
-    await mongoose.connect(process.env['MONGO_URI']);
 });
 
 afterAll(async () => {
-    await mongoose.disconnect();
 });
 
 jest.mock('@/app/config/databaseConnection', () => ({
