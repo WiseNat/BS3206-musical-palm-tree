8a674003c81bd3b6d8d8fec8696367fa9d2327b3
Tom Shortridge <tomshortridge@toms-mbp-2.lan>
2024-05-20 14:00:30 +0100

    fix: InnoConnect spelling

--- innoconnect-ui/src/app/page.js
+++ innoconnect-ui/src/app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
       <div className="flex items-center">
         <Image src="/inno-logo.svg" width="100" height="100" />
         <h1 className="flex justify-center items-center text-9xl">
-          Innoconnect
+          InnoConnect
         </h1>
       </div>
       <div className="object-left">
