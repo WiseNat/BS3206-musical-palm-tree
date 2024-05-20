43aad87a2512cf3dcb91aab17deb08e9365ebdbc
Tom Shortridge <tomshortridge@toms-mbp-2.lan>
2024-05-20 11:24:35 +0100

    feat: updating readme

--- innoconnect-ui/src/app/page.js
+++ innoconnect-ui/src/app/page.js
@@ -7,12 +7,12 @@ export default function Home() {
     <main>
       <Navbar />
       <div className="mx-16 my-9 flex flex-col items-center space-y-4 align-middle">
-          <div className="flex items-center">
-            <Image src="/inno-logo.svg" width="100" height="100" />
-            <h1 className="flex justify-center items-center text-9xl">
-              Innoconnect
-            </h1>
-          </div>
+        <div className="flex items-center">
+          <Image src="/inno-logo.svg" width="100" height="100" />
+          <h1 className="flex justify-center items-center text-9xl">
+            Innoconnect
+          </h1>
+        </div>
         <div className="object-left">
           <p className="p-5">
             Empower Your Ideas, Connect for Impact - InnoConnect: Building
